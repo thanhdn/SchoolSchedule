2f471df443dc70fe72cd5c3f2fb4423a870a186b
thanhdn <dngthanh@hotmail.com>
2017-10-16 20:17:48 +0700

    Second commit

--- src/main/java/com/tlsoft/GradeController.java
+++ src/main/java/com/tlsoft/GradeController.java
@@ -1,5 +1,10 @@
 package com.tlsoft;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.collections.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,25 +16,33 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tlsoft.entities.Grade;
 import com.tlsoft.repository.GradeRepository;
 
-@RestController  
-@RequestMapping(path="/grade")
+
+
+
+@RestController
+@RequestMapping(path = "/grade")
 public class GradeController {
-	
-	 @Autowired 
-	 private GradeRepository gradeRepository;
-	 
-	 
-	@PostMapping(path="/add") // Map ONLY GET Requests
-	public @ResponseBody Grade addNewGrade (@RequestBody Grade grade) {
+
+	@Autowired
+	private GradeRepository gradeRepository;
+
+	@PostMapping(path = "/add") // Map ONLY GET Requests
+	public @ResponseBody Grade addNewGrade(@RequestBody Grade grade) {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
 
-		gradeRepository.save(grade);
-		return grade;
+		Grade _grade = gradeRepository.save(grade);
+		return _grade;
 	}
-	
-	@GetMapping(path="/view")
-	public @ResponseBody Grade getGrade() {
-		return new Grade();
+
+	@SuppressWarnings("unchecked")
+	@GetMapping(path = "/view")
+	public @ResponseBody List<Grade> getGrade() {
+		Iterable<Grade> gradeItr = gradeRepository.findAll();
+		Iterator<Grade> grades = gradeItr.iterator();
+		if(grades != null) {
+			return IteratorUtils.toList(grades);
+		}
+		return new ArrayList<Grade>();
 	}
 }
