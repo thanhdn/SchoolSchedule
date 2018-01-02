6bdee39fd459cb28d10fdff2c42c570391dcd808
thanhdn <dngthanh@hotmail.com>
2018-01-02 22:23:08 +0700

    revert

--- src/main/java/com/tlsoft/GradeController.java
+++ src/main/java/com/tlsoft/GradeController.java
@@ -6,15 +6,20 @@ import java.util.List;
 
 import org.apache.commons.collections.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tlsoft.entities.Grade;
+import com.tlsoft.entities.InputObj;
 import com.tlsoft.repository.GradeRepository;
+
+import net.minidev.json.JSONObject;
 
 
 
@@ -25,24 +30,46 @@ public class GradeController {
 
 	@Autowired
 	private GradeRepository gradeRepository;
-
-	@PostMapping(path = "/add") // Map ONLY GET Requests
-	public @ResponseBody Grade addNewGrade(@RequestBody Grade grade) {
+	
+	
+	public @ResponseBody Grade deleteGrade(@RequestBody Grade grade) {
+		return null;
+	}
+	
+	
+	@PostMapping(path = "/req") // Map ONLY GET Requests
+	//@PutMapping(path = "/add")
+	public @ResponseBody Grade addNewGrade(@RequestBody InputObj input) {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
-
-		Grade _grade = gradeRepository.save(grade);
+		Grade _grade = null;
+		String action = input.getAction();
+		if(!StringUtils.isEmpty(action)) {
+			_grade  = input.getGrade();
+			if("REM".equals(action)) {
+				gradeRepository.delete(_grade);
+			} else {
+				gradeRepository.save(_grade);
+			}
+		}
 		return _grade;
 	}
 
 	@SuppressWarnings("unchecked")
-	@GetMapping(path = "/view")
+	@GetMapping(path = "/serving")
 	public @ResponseBody List<Grade> getGrade() {
 		Iterable<Grade> gradeItr = gradeRepository.findAll();
 		Iterator<Grade> grades = gradeItr.iterator();
 		if(grades != null) {
 			return IteratorUtils.toList(grades);
 		}
+		
+		/*SortedMap<String, String> a = new TreeMap<String, String>();
+		Set<String> m = new LinkedHashSet<String>();*/
+		
+		
 		return new ArrayList<Grade>();
+		
 	}
+	
 }
--- src/main/java/com/tlsoft/entities/Grade.java
+++ src/main/java/com/tlsoft/entities/Grade.java
@@ -79,4 +79,67 @@ public class Grade {
 	public void setCreator(String creator) {
 		this.creator = creator;
 	}
+
+	/*@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((creationDate == null) ? 0 : creationDate.hashCode());
+		result = prime * result + ((creator == null) ? 0 : creator.hashCode());
+		result = prime * result + ((gradeId == null) ? 0 : gradeId.hashCode());
+		result = prime * result + ((gradeName == null) ? 0 : gradeName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lectureNumber == null) ? 0 : lectureNumber.hashCode());
+		result = prime * result + ((updateDate == null) ? 0 : updateDate.hashCode());
+		return result;
+	}*/
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Grade other = (Grade) obj;
+		if (creationDate == null) {
+			if (other.creationDate != null)
+				return false;
+		} else if (!creationDate.equals(other.creationDate))
+			return false;
+		if (creator == null) {
+			if (other.creator != null)
+				return false;
+		} else if (!creator.equals(other.creator))
+			return false;
+		if (gradeId == null) {
+			if (other.gradeId != null)
+				return false;
+		} else if (!gradeId.equals(other.gradeId))
+			return false;
+		if (gradeName == null) {
+			if (other.gradeName != null)
+				return false;
+		} else if (!gradeName.equals(other.gradeName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lectureNumber == null) {
+			if (other.lectureNumber != null)
+				return false;
+		} else if (!lectureNumber.equals(other.lectureNumber))
+			return false;
+		if (updateDate == null) {
+			if (other.updateDate != null)
+				return false;
+		} else if (!updateDate.equals(other.updateDate))
+			return false;
+		return true;
+	}
+	
+	
 }
